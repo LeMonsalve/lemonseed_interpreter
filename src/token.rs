5d8bb25e4b68d7cd254c80bdcf2a02c79f58bc89
Juan José Monsalve Orozco <devlemonsalve@gmail.com>
2024-11-28 12:01:21 -0500

    Implement Token struct constructor and Display trait for TokenKind

--- src/token.rs
+++ src/token.rs
@@ -1,3 +1,5 @@
+use std::fmt::{Display, Formatter, Result};
+
 #[derive(Debug, PartialEq)]
 pub struct Token {
     pub kind: TokenKind,
@@ -26,4 +28,31 @@ pub enum TokenKind {
 
     Function,
     Var
+}
+
+impl Token {
+    pub fn new<S: Into<String>>(kind: TokenKind, literal: S) -> Token {
+        Token { kind, literal: literal.into() }
+    }
+}
+
+impl Display for TokenKind {
+    fn fmt(&self, f: &mut Formatter<'_>) -> Result {
+        match self {
+            TokenKind::Illegal => write!(f, "Illegal"),
+            TokenKind::Eof => write!(f, "Eof"),
+            TokenKind::Ident => write!(f, "Ident"),
+            TokenKind::Int => write!(f, "Int"),
+            TokenKind::Assign => write!(f, "="),
+            TokenKind::Plus => write!(f, "+"),
+            TokenKind::Comma => write!(f, ","),
+            TokenKind::Semicolon => write!(f, ";"),
+            TokenKind::LeftParenthesis => write!(f, "("),
+            TokenKind::RightParenthesis => write!(f, ")"),
+            TokenKind::LeftBrace => write!(f, "{{"),
+            TokenKind::RightBrace => write!(f, "}}"),
+            TokenKind::Function => write!(f, "Function"),
+            TokenKind::Var => write!(f, "Var"),
+        }
+    }
 }
