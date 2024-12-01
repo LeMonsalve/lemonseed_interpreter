e22ac08f8b3e4025d4c03746ba3bcc5b5774325d
Juan José Monsalve Orozco <devlemonsalve@gmail.com>
2024-12-01 18:19:55 -0500

    Refactor TokenKind enum and update Display implementation for improved clarity

--- src/token.rs
+++ src/token.rs
@@ -9,30 +9,56 @@ pub struct Token {
 #[derive(Debug, PartialEq)]
 pub enum TokenKind {
     Illegal,
-    Eof,
+    EOF,
 
-    Ident,
+    Identifier,
     Int,
 
     Assign,
     Plus,
+    Minus,
+    Bang,
+    Asterisk,
+    Slash,
+
+    LessThan,
+    GreaterThan,
+
+    Equal,
+    NotEqual,
 
     Comma,
     Semicolon,
 
     LeftParenthesis,
     RightParenthesis,
-
     LeftBrace,
     RightBrace,
+    LeftBracket,
+    RightBracket,
 
     Function,
-    Var
+    Variable,
+    Constant,
 }
 
 impl Token {
     pub fn new<S: Into<String>>(kind: TokenKind, literal: S) -> Token {
-        Token { kind, literal: literal.into() }
+        Token {
+            kind,
+            literal: literal.into(),
+        }
+    }
+}
+
+impl TokenKind {
+    pub fn lookup_identifier<S: AsRef<str>>(literal: &S) -> TokenKind {
+        match literal.as_ref() {
+            "function" => TokenKind::Function,
+            "var" => TokenKind::Variable,
+            "const" => TokenKind::Constant,
+            _ => TokenKind::Identifier,
+        }
     }
 }
 
@@ -40,19 +66,30 @@ impl Display for TokenKind {
     fn fmt(&self, f: &mut Formatter<'_>) -> Result {
         match self {
             TokenKind::Illegal => write!(f, "Illegal"),
-            TokenKind::Eof => write!(f, "Eof"),
-            TokenKind::Ident => write!(f, "Ident"),
+            TokenKind::EOF => write!(f, "EOF"),
+            TokenKind::Identifier => write!(f, "Identifier"),
             TokenKind::Int => write!(f, "Int"),
             TokenKind::Assign => write!(f, "="),
             TokenKind::Plus => write!(f, "+"),
+            TokenKind::Minus => write!(f, "-"),
+            TokenKind::Bang => write!(f, "!"),
+            TokenKind::Asterisk => write!(f, "*"),
+            TokenKind::Slash => write!(f, "/"),
+            TokenKind::LessThan => write!(f, "<"),
+            TokenKind::GreaterThan => write!(f, ">"),
+            TokenKind::Equal => write!(f, "=="),
+            TokenKind::NotEqual => write!(f, "!="),
             TokenKind::Comma => write!(f, ","),
             TokenKind::Semicolon => write!(f, ";"),
             TokenKind::LeftParenthesis => write!(f, "("),
             TokenKind::RightParenthesis => write!(f, ")"),
             TokenKind::LeftBrace => write!(f, "{{"),
             TokenKind::RightBrace => write!(f, "}}"),
+            TokenKind::LeftBracket => write!(f, "["),
+            TokenKind::RightBracket => write!(f, "]"),
             TokenKind::Function => write!(f, "Function"),
-            TokenKind::Var => write!(f, "Var"),
+            TokenKind::Variable => write!(f, "Variable"),
+            TokenKind::Constant => write!(f, "Constant"),
         }
     }
 }
